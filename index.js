0e4e53d578914338153106eba0597c40051adbf3
Aadesh-1999 <62835473+Aadesh-1999@users.noreply.github.com>
2022-02-13 23:22:28 +0530

    songs, play pause, events

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 console.log("Welcome to spotify");
 let songIndex=0;
-let audioelement=new Audio('./Resources/songs/1.mp3');
+let audioElement=new Audio('./Resources/songs/1.mp3');
 let masterPlay=document.getElementById('masterPlay');
 let myProgressBar=document.getElementById("myProgressBar");
 let gif=document.getElementById("gif");
+let songInfo=document.getElementsByClassName("songInfo")[0].getElementsByTagName("h3")[0];
 
 let songs=[
-    {SongName:"Song 1", filePath:"./Resources/songs/1.mp3", covePath:"./Resources/covers/1.png"},
-    {SongName:"Song 1", filePath:"./Resources/songs/1.mp3", covePath:"./Resources/covers/1.png"},
-    {SongName:"Song 1", filePath:"./Resources/songs/1.mp3", covePath:"./Resources/covers/1.png"},
-    {SongName:"Song 1", filePath:"./Resources/songs/1.mp3", covePath:"./Resources/covers/1.png"},
-    {SongName:"Song 1", filePath:"./Resources/songs/1.mp3", covePath:"./Resources/covers/1.png"},
-    {SongName:"Song 1", filePath:"./Resources/songs/1.mp3", covePath:"./Resources/covers/1.png"},
-    {SongName:"Song 1", filePath:"./Resources/songs/1.mp3", covePath:"./Resources/covers/1.png"}
+    {SongName:"Airanichya Deva Tula...", filePath:"./Resources/songs/1.mp3", covePath:"./Resources/covers/1.png"},
+    {SongName:"Song 2", filePath:"./Resources/songs/2.wav", covePath:"./Resources/covers/1.png"},
+    {SongName:"Song 3", filePath:"./Resources/songs/3.wav", covePath:"./Resources/covers/1.png"},
+    {SongName:"Song 4", filePath:"./Resources/songs/4.wav", covePath:"./Resources/covers/1.png"},
+    {SongName:"Song 5", filePath:"./Resources/songs/5.wav", covePath:"./Resources/covers/1.png"},
+    {SongName:"Song 6", filePath:"./Resources/songs/6.wav", covePath:"./Resources/covers/1.png"},
+    {SongName:"Song 7", filePath:"./Resources/songs/7.wav", covePath:"./Resources/covers/1.png"},
+    {SongName:"Song 8", filePath:"./Resources/songs/8.wav", covePath:"./Resources/covers/1.png"}
 ];
 
+let songsPlayBtns=document.getElementsByClassName("songPlayBtn");
+console.log(songsPlayBtns);
 
-//audioElement.play
-let audioElement=new Audio('./Resources/song1.mp3');
+Array.from(songsPlayBtns).forEach((songPlayBtn,index) => {
+    songPlayBtn.addEventListener('click',()=>{
+        audioElement.pause();
+        masterPlay.className="fa-solid fa-3x fa-circle-play";
+        gif.style.opacity=0;
+        audioElement=new Audio(songs[index].filePath);
+        audioElement.play();
+        masterPlay.className="fa-solid fa-3x fa-circle-pause";
+        gif.style.opacity=1;
+        songInfo.innerText=songs[index].SongName;
+    })
+});
 
 //Handle play/pause click
 masterPlay.addEventListener('click',()=>{
@@ -36,16 +50,10 @@ let progress=0;
 //Listen to events
 audioElement.addEventListener('timeupdate',()=>{
     console.log("timeUpdate");
-    //console.log(audioElement.currentTime);
-    //console.log(audioElement.duration);
     progress=parseInt((audioElement.currentTime/audioElement.duration)*100);
-    //console.log(progress);
     myProgressBar.value=progress;
 })
 
 myProgressBar.addEventListener('change',()=>{
     audioElement.currentTime=myProgressBar.value*audioElement.duration/100;
-    
-    //console.log(progress);
- 
 })
